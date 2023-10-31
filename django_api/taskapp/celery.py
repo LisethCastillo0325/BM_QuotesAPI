9bc396cc668c49dd3365c4cccb87c979ca6f32de
LisethCastillo0325 <lisethcastillo.0325@gmail.com>
2023-10-31 14:18:40 -0500

    code adjustments

--- django_api/taskapp/celery.py
+++ django_api/taskapp/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-from celery.schedules import crontab
+# from celery.schedules import crontab
 
 
 # Set the default Django settings module for the 'celery' program.
@@ -20,11 +20,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'verificar-facturacion-servicios-cada-dia': {
-        'task': 'facturacion_servicios',
-        'schedule': crontab(minute=0, hour=6),
-        'args': ()
-    },
+    # 'verificar-facturacion-servicios-cada-dia': {
+    #     'task': 'facturacion_servicios',
+    #     'schedule': crontab(minute=0, hour=6),
+    #     'args': ()
+    # },
 }
 
 app.conf.timezone = 'America/Bogota'
